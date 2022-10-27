4876d89b82d298af98b02661e615f4b096c4275c
Matias Aitolahti <96495033+Real-bay@users.noreply.github.com>
2022-10-27 23:51:42 +0300

    Updated pom.xml and module-info.java

--- roadwatch/src/main/java/module-info.java
+++ roadwatch/src/main/java/module-info.java
@@ -6,6 +6,10 @@ module fi.tuni.roadwatch {
     requires org.kordamp.bootstrapfx.core;
     requires com.fasterxml.jackson.dataformat.xml;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires org.apache.httpcomponents.client5.httpclient5;
+    requires org.apache.httpcomponents.core5.httpcore5;
+    requires org.apache.commons.io;
     requires java.xml;
 
     opens fi.tuni.roadwatch to javafx.fxml;
