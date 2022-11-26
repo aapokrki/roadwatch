145e284ce39c888764407dde66441daa6d7659b3
Matias Aitolahti <96495033+Real-bay@users.noreply.github.com>
2022-11-26 22:57:31 +0200

    Restructured RoadData to match improved dataclass format

--- roadwatch/src/main/java/fi/tuni/roadwatch/RoadData.java
+++ roadwatch/src/main/java/fi/tuni/roadwatch/RoadData.java
@@ -12,104 +12,156 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+// Data class for road condition data from Digitraffic
 public class RoadData {
 
-    public Date dataUpdatedTime;
-    public ArrayList<WeatherData> weatherData;
+    Date dataUpdatedTime;
+    ArrayList<roadWeatherData> roadWeatherData;
+    @JsonProperty("dataUpdatedTime")
+    public Date getDataUpdatedTime() {
+        return this.dataUpdatedTime; }
+    public void setDataUpdatedTime(Date dataUpdatedTime) {
+        this.dataUpdatedTime = dataUpdatedTime; }
 
-    public class WeatherData {
-        public String id;
-        public ArrayList<RoadCondition> roadConditions;
-    }
+    @JsonProperty("WeatherData")
+    public ArrayList<roadWeatherData> getRoadWeatherData() {
+        return this.roadWeatherData; }
+    public void setroadWeatherData(ArrayList<roadWeatherData> roadWeatherData) {
+        this.roadWeatherData = roadWeatherData; }
 
-    public class RoadCondition {
-        public Date time;
-        public String type;
-        public String forecastName;
-        public boolean daylight;
-        public String roadTemperature;
-        public String temperature;
-        public double windSpeed;
-        public int windDirection;
-        public String overallRoadCondition;
-        public String weatherSymbol;
-        public String reliability;
-        public ForecastConditionReason forecastConditionReason;
-    }
+    public class roadWeatherData{
 
-    public class ForecastConditionReason {
-        public String precipitationCondition;
-        public String roadCondition;
-    }
+        String id;
+        ArrayList<RoadCondition> roadConditions;
+        @JsonProperty("id")
+        public String getId() {
+            return this.id; }
+        public void setId(String id) {
+            this.id = id; }
 
-    // Constructor with parsing
-    public RoadData(JsonNode json) throws ParseException {
-        ObjectMapper mapper = new ObjectMapper();
-        this.dataUpdatedTime = stringToDate(json.get("dataUpdatedTime").asText());
-        this.weatherData = readWeatherData(json.get("weatherData"));
-        System.out.println("RoadData created");
-    }
-
-    // Parsing each weatherData
-    private ArrayList<WeatherData> readWeatherData(JsonNode weatherData) {
-        ArrayList<WeatherData> wd = new ArrayList<>();
-        for (JsonNode node : weatherData) {
-            WeatherData w = new WeatherData();
-            w.id = node.get("id").asText();
-            w.roadConditions = readRoadConditions(node.get("roadConditions"));
-            wd.add(w);
-        }
-        return wd;
-    }
-
-    // Parsing each roadCondition
-    private ArrayList<RoadCondition> readRoadConditions(JsonNode roadConditions) {
-        ArrayList<RoadCondition> rc = new ArrayList<>();
-        for (JsonNode node : roadConditions) {
-            RoadCondition r = new RoadCondition();
-            try {
-                r.time = stringToDate(node.get("time").asText());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            r.type = node.get("type").asText();
-            r.forecastName = node.get("forecastName").asText();
-            r.daylight = node.get("daylight").asBoolean();
-            r.roadTemperature = node.get("roadTemperature").asText();
-            r.temperature = node.get("temperature").asText();
-            r.windSpeed = node.get("windSpeed").asDouble();
-            r.windDirection = node.get("windDirection").asInt();
-            r.overallRoadCondition = node.get("overallRoadCondition").asText();
-            r.weatherSymbol = node.get("weatherSymbol").asText();
-            r.reliability = node.get("reliability").asText();
-            try {
-                r.forecastConditionReason = readForecastConditionReason(node.get("forecastConditionReason"));
-            } catch (NullPointerException e) {
-                continue;
-            }
-
-            rc.add(r);
-        }
-        return rc;
-    }
-
-    // Parsing forecastConditionReason
-    private ForecastConditionReason readForecastConditionReason(JsonNode forecastConditionReason) {
-        ForecastConditionReason fcr = new ForecastConditionReason();
-        fcr.precipitationCondition = forecastConditionReason.get("precipitationCondition").asText();
-        fcr.roadCondition = forecastConditionReason.get("roadCondition").asText();
-        return fcr;
-    }
-
-    // Assistant function for parsing date
-    private Date stringToDate(String s) throws ParseException {
-        try {
-            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(s);
-        }
-        catch (ParseException e) {
-            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(s);
-        }
+        @JsonProperty("roadConditions")
+        public ArrayList<RoadCondition> getRoadConditions() {
+            return this.roadConditions; }
+        public void setRoadConditions(ArrayList<RoadCondition> roadConditions) {
+            this.roadConditions = roadConditions; }
 
     }
 
+    public class RoadCondition{
+
+        Date time;
+        String type;
+        String forecastName;
+        boolean daylight;
+        String roadTemperature;
+        String temperature;
+        int windSpeed;
+        int windDirection;
+        String overallRoadCondition;
+        String weatherSymbol;
+        String reliability;
+        ForecastConditionReason forecastConditionReason;
+        @JsonProperty("time")
+        public Date getTime() {
+            return this.time; }
+        public void setTime(Date time) {
+            this.time = time; }
+
+        @JsonProperty("type")
+        public String getType() {
+            return this.type; }
+        public void setType(String type) {
+            this.type = type; }
+
+        @JsonProperty("forecastName")
+        public String getForecastName() {
+            return this.forecastName; }
+        public void setForecastName(String forecastName) {
+            this.forecastName = forecastName; }
+
+        @JsonProperty("daylight")
+        public boolean getDaylight() {
+            return this.daylight; }
+        public void setDaylight(boolean daylight) {
+            this.daylight = daylight; }
+
+        @JsonProperty("roadTemperature")
+        public String getRoadTemperature() {
+            return this.roadTemperature; }
+        public void setRoadTemperature(String roadTemperature) {
+            this.roadTemperature = roadTemperature; }
+
+        @JsonProperty("temperature")
+        public String getTemperature() {
+            return this.temperature; }
+        public void setTemperature(String temperature) {
+            this.temperature = temperature; }
+
+        @JsonProperty("windSpeed")
+        public int getWindSpeed() {
+            return this.windSpeed; }
+        public void setWindSpeed(int windSpeed) {
+            this.windSpeed = windSpeed; }
+
+        @JsonProperty("windDirection")
+        public int getWindDirection() {
+            return this.windDirection; }
+        public void setWindDirection(int windDirection) {
+            this.windDirection = windDirection; }
+
+        @JsonProperty("overallRoadCondition")
+        public String getOverallRoadCondition() {
+            return this.overallRoadCondition; }
+        public void setOverallRoadCondition(String overallRoadCondition) {
+            this.overallRoadCondition = overallRoadCondition; }
+
+        @JsonProperty("weatherSymbol")
+        public String getWeatherSymbol() {
+            return this.weatherSymbol; }
+        public void setWeatherSymbol(String weatherSymbol) {
+            this.weatherSymbol = weatherSymbol; }
+
+        @JsonProperty("reliability")
+        public String getReliability() {
+            return this.reliability; }
+        public void setReliability(String reliability) {
+            this.reliability = reliability; }
+
+        @JsonProperty("forecastConditionReason")
+        public ForecastConditionReason getForecastConditionReason() {
+            return this.forecastConditionReason; }
+        public void setForecastConditionReason(ForecastConditionReason forecastConditionReason) {
+            this.forecastConditionReason = forecastConditionReason; }
+    }
+
+    public class ForecastConditionReason{
+
+        String precipitationCondition;
+        String roadCondition;
+        String frictionCondition;
+        String windCondition;
+        @JsonProperty("precipitationCondition")
+        public String getPrecipitationCondition() {
+            return this.precipitationCondition; }
+        public void setPrecipitationCondition(String precipitationCondition) {
+            this.precipitationCondition = precipitationCondition; }
+
+        @JsonProperty("roadCondition")
+        public String getRoadCondition() {
+            return this.roadCondition; }
+        public void setRoadCondition(String roadCondition) {
+            this.roadCondition = roadCondition; }
+
+        @JsonProperty("frictionCondition")
+        public String getFrictionCondition() {
+            return this.frictionCondition; }
+        public void setFrictionCondition(String frictionCondition) {
+            this.frictionCondition = frictionCondition; }
+
+        @JsonProperty("windCondition")
+        public String getWindCondition() {
+            return this.windCondition; }
+        public void setWindCondition(String windCondition) {
+            this.windCondition = windCondition; }
+    }
 }
