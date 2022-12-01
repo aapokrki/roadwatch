8c5489a99aad26bbe761591c0fbca572408d08d2
Ronja <ronja.lipsonen@tuni.fi>
2022-12-01 20:07:06 +0200

    Preferences started.

--- roadwatch/src/main/java/fi/tuni/roadwatch/PreferencesController.java
+++ roadwatch/src/main/java/fi/tuni/roadwatch/PreferencesController.java
@@ -2,14 +2,31 @@ package fi.tuni.roadwatch;
 
 import fi.tuni.roadwatch.SessionData;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
+import java.util.Objects;
+
 public class PreferencesController {
+    private String locationPreference;
+    private String weatherPreference;
+    private String roadDataPreference;
 
     @FXML
-    private Label preferencesLabel;
+    private Label locationLabel;
+    @FXML
+    private ComboBox<String> weatherComboBox;
+    @FXML
+    private ComboBox<String> conditionTypeComboBox;
     private SessionData sessionData;
     public void setSessionData(SessionData sessionData) {
         this.sessionData = sessionData;
+    }
+
+    public void changeWeatherType() {
+        weatherPreference = weatherComboBox.getValue();
+    }
+    public void changeConditionType() {
+        roadDataPreference = conditionTypeComboBox.getValue();
     }
 }
