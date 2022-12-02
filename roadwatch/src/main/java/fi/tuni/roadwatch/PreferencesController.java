9a9885c14fa824aba848f35d0ff148deb9b1a85a
Ronja <ronja.lipsonen@tuni.fi>
2022-12-02 11:37:27 +0200

    Preferences UX updated.

--- roadwatch/src/main/java/fi/tuni/roadwatch/PreferencesController.java
+++ roadwatch/src/main/java/fi/tuni/roadwatch/PreferencesController.java
@@ -1,6 +1,8 @@
 package fi.tuni.roadwatch;
 
 import fi.tuni.roadwatch.SessionData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -11,22 +13,30 @@ public class PreferencesController {
     private String locationPreference;
     private String weatherPreference;
     private String roadDataPreference;
+    private String maintenancePreference;
 
+    // TODO: Aapo linkkaa karttaan tai sit poistan.
     @FXML
     private Label locationLabel;
     @FXML
     private ComboBox<String> weatherComboBox;
     @FXML
     private ComboBox<String> conditionTypeComboBox;
+    @FXML
+    public ComboBox<String> maintenanceTaskCombobox;
     private SessionData sessionData;
+
     public void setSessionData(SessionData sessionData) {
         this.sessionData = sessionData;
+
+        ObservableList<String> taskTypesObservable= FXCollections.observableArrayList(sessionData.taskTypes);
+        taskTypesObservable.add(0,"ALL");
+        maintenanceTaskCombobox.setItems(taskTypesObservable);
     }
 
-    public void changeWeatherType() {
+    public void setPreferences() {
         weatherPreference = weatherComboBox.getValue();
-    }
-    public void changeConditionType() {
         roadDataPreference = conditionTypeComboBox.getValue();
+        maintenancePreference = maintenanceTaskCombobox.getValue();
     }
 }
